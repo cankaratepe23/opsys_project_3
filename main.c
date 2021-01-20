8058998937416e5bd6a30b7ae8e8784db11d6548
cankaratepe23 <cankaratepe2000@gmail.com>
2021-01-20 18:16:23 +0300

    Refactor queue variables, fix resize algorithm, implement Book struct.

--- main.c
+++ main.c
@@ -22,20 +22,31 @@ typedef struct publisher {
     sem_t full;
     sem_t empty;
     pthread_mutex_t lock;
-    int in;
-    int out;
-    int totalBookCount;
+    int rear;
+    int front;
+    int total_book_count;
 } Publisher;
+
+typedef struct book {
+    int bookid;
+    int typeid;
+} Book;
+
+void init_book(Publisher *publisher, Book **out) {
+    (*out) = malloc(sizeof(Book));
+    (*out)->bookid = ++publisher->total_book_count;
+    (*out)->typeid = publisher->id;
+}
 
 // Initialize a single publisher type with the current global variables.
 void init_buffer(Publisher *publisher) {
     printf("DEBUG: Allocating memory for buffer %d\n", publisher->id);
     publisher->buflen = buffer_size;
-    publisher->buffer = calloc(0, sizeof(void *) * publisher->buflen);
-    printf("DEBUG: Initializing in/out values and book count for buffer %d", publisher->id);
-    publisher->in = 0;
-    publisher->out = 0;
-    publisher->totalBookCount = 0;
+    publisher->buffer = calloc(0, sizeof(Book *) * publisher->buflen);
+    printf("DEBUG: Initializing rear/front values and book count for buffer %d", publisher->id);
+    publisher->rear = 0;
+    publisher->front = 0;
+    publisher->total_book_count = 0;
     printf("DEBUG: Initializing fullsem for buffer %d.\n", publisher->id);
     sem_init(&publisher->full, 0, 0);
     printf("DEBUG: Initializing emptysem for buffer %d.\n", publisher->id);
@@ -45,7 +56,7 @@ void init_buffer(Publisher *publisher) {
 }
 
 // Create and initialize all publisher types
-void init(Publisher ** out) {
+void init(Publisher **out) {
     for (int i = 0; i < publisher_type; ++i) {
         printf("DEBUG: Initializing values for publisher type %d\n", i);
         Publisher *publisher = malloc(sizeof(Publisher));
@@ -60,6 +71,8 @@ void init(Publisher ** out) {
 void resizebuffer(Publisher *publisher, int oldsize) {
     // We first copy the current buffer's address to a temporary variable, so that it's not lost.
     int **tempbuffer = (int **)publisher->buffer;
+    int oldrear = publisher->rear;
+    int oldfront = publisher->front;
     // Allocate new memory for the buffer and set the buflen variable to the new size.
     publisher->buffer = calloc(0, (sizeof (void *)) * (oldsize * 2));
     publisher->buflen = oldsize * 2;
@@ -68,12 +81,25 @@ void resizebuffer(Publisher *publisher, int oldsize) {
     sem_init(&publisher->empty, 0, oldsize);
     // TODO Implement a buffercpy method here?
     // Copy the contents of the old buffer to the newly allocated one.
-    for (int i = 0; i < oldsize; ++i) {
+    for (int i = 0; i <= oldrear; i++) {
+        // Copy from the front of the old buffer until the rear is reached.
         publisher->buffer[i] = tempbuffer[i];
+    }
+    if (oldfront > oldrear) {
+        // The list goes over the 'seam' of the circular buffer; we need to copy from the end of the old buffer as well.
+        int newfront = publisher->buflen - (oldsize - oldfront);
+        int old_buffer_index = oldfront;
+        int new_buffer_index = newfront;
+        while (old_buffer_index < oldsize) {
+            publisher->buffer[new_buffer_index] = tempbuffer[old_buffer_index];
+            old_buffer_index++;
+            new_buffer_index++;
+        }
+        publisher->front = newfront;
     }
 }
 
-void enqueue(Publisher *publisher, void *value) {
+void enqueue(Publisher *publisher, Book *value) {
     printf("DEBUG: Waiting for empty spaces (if needed)\n");
     int semval = 0;
     sem_getvalue(&publisher->empty, &semval);
@@ -91,34 +117,33 @@ void enqueue(Publisher *publisher, void *value) {
     printf("DEBUG: Locking mutex for enqueue\n");
     // Lock the mutex for the buffer before writing to it.
     pthread_mutex_lock(&publisher->lock);
-    int bufferwriteindex = (publisher->in++) % publisher->buflen;
+    int bufferwriteindex = (publisher->rear++) % publisher->buflen;
     if (bufferwriteindex < 0) {
         fprintf(stderr, "ERROR: Integer was already overflowed when trying to queue value to buffer %d\n", publisher->id);
         exit(-1);
     }
-    printf("DEBUG: Writing value %d to buffer %d position %d\n", *(int *)value, publisher->id, bufferwriteindex);
+    printf("DEBUG: Adding Book%d_%d to buffer %d position %d\n", value->typeid, value->bookid, publisher->id, bufferwriteindex);
     publisher->buffer[bufferwriteindex] = value;
-    publisher->totalBookCount++;
     printf("DEBUG: Unlocking mutex after enqueue\n");
     // Unlock the mutex after writing to the buffer is done.
     pthread_mutex_unlock(&publisher->lock);
     printf("DEBUG: Updating semaphore\n");
-    // Update the full semaphore value to reflect the number of full slots in the buffer, or the number of books in the buffer
+    // Update the full semaphore value to reflect the number of full slots in the buffer, or the number of books rear the buffer
     sem_post(&publisher->full);
 }
 
-void *dequeue(Publisher *publisher) {
+Book *dequeue(Publisher *publisher) {
     printf("DEBUG: Waiting for items in buffer (if needed)\n");
     sem_wait(&publisher->full); // Waits if there are no items left in the buffer.
     printf("DEBUG: Locking mutex for dequeue\n");
     pthread_mutex_lock(&publisher->lock);
-    int bufferreadindex = (publisher->out++) % publisher->buflen;
+    int bufferreadindex = (publisher->front++) % publisher->buflen;
     if (bufferreadindex < 0) {
         printf("Integer was already overflowed when trying to queue value to buffer %d\n", publisher->id);
         exit(-1);
     }
     printf("DEBUG: Reading value from buffer %d at position %d\n", publisher->id, bufferreadindex);
-    void *result = publisher->buffer[bufferreadindex];
+    Book *result = publisher->buffer[bufferreadindex];
     printf("DEBUG: Unlocking mutex after dequeue\n");
     pthread_mutex_unlock(&publisher->lock);
     printf("DEBUG: Updating semaphore\n");
@@ -133,7 +158,7 @@ void publish(Publisher *publisher) {
 int main(int argc, char *argv[]) {
     if (argc != 10) {
         fprintf(stderr,
-                "Usage: project3.out {-n publisher_type publisher_thread_count packager_thread_count} {-b books_per_thread} {-s package_size buffer_size}");
+                "Usage: project3 {-n publisher_type publisher_thread_count packager_thread_count} {-b books_per_thread} {-s package_size buffer_size}");
         exit(-1);
     }
     int argcounter = 0;
@@ -152,35 +177,18 @@ int main(int argc, char *argv[]) {
         }
         else {
             fprintf(stderr,
-                    "Usage: project3.out {-n publisher_type publisher_thread_count packager_thread_count} {-b books_per_thread} {-s package_size buffer_size}");
+                    "Usage: project3 {-n publisher_type publisher_thread_count packager_thread_count} {-b books_per_thread} {-s package_size buffer_size}");
             exit(-1);
         }
     }
     Publisher *publishers[publisher_type];
     init(publishers);
 
-    int val1 = 10;
-    int val2 = 20;
-    int val3 = 30;
-    int val4 = 40;
-    int val5 = 50;
-    int val6 = 60;
-    int val7 = 70;
-    int val8 = 80;
-    int val9 = 90;
-    int val10 = 100;
-    int val11 = 110;
-    enqueue(publishers[0], &val1);
-    enqueue(publishers[0], &val2);
-    enqueue(publishers[0], &val3);
-    enqueue(publishers[0], &val4);
-    enqueue(publishers[0], &val5);
-    enqueue(publishers[0], &val6);
-    enqueue(publishers[0], &val7);
-    enqueue(publishers[0], &val8);
-    enqueue(publishers[0], &val9);
-    enqueue(publishers[0], &val10);
-    enqueue(publishers[0], &val11);
+    Book *books[11];
+    for (int i = 0; i < 11; ++i) {
+        init_book(publishers[0], &books[i]);
+        enqueue(publishers[0], books[i]);
+    }
 
     printf("Execution complete.\n");
     fprintf(stderr, "Error test.\n");
